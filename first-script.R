cd0583c573c58ceacad8952a1e03c0044ae4be2c
Griffin Hundley <hundlegq@dukes.jmu.edu>
2021-02-10 15:57:48 -0500

    add script

---
+++ first-script.R
@@ -0,0 +1,9 @@
+2+2
+my_number <- 2+2
+my_number * 2
+coffees <- c(2,1,2,3,1,2,0,2,3,1)
+adjusted_coffees <- coffees * 1.2
+coffee_data <- data.frame(coffees, adjusted_coffees)
+coffee_data[1,]
+coffee_data[,1]
+coffee_data$coffees[1]
