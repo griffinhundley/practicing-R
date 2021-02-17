a3c615d975df30eee8b25aacb5d7e4d0aa2b4958
Griffin Hundley <hundlegq@dukes.jmu.edu>
2021-02-17 09:14:32 -0500

    distplot by broadcaster type

--- vis-repro.R
+++ vis-repro.R
@@ -4,7 +4,9 @@ library(dplyr)
 
 df <- readr::read_csv('data/streamer_data.csv')
 
-ggplot2::ggplot(data = df,
-                aes(x = active_days_per_week)) + 
-  geom_histogram() +
-  facet_wrap(~target)
+ggplot(df, aes(x=active_days_per_week, fill=broadcaster_type)) + 
+  geom_density(alpha=.3) +
+  labs(title = 'Days of Activity per Week',
+       subtitle = 'By broadcaster type',
+       x = 'number of days per week',
+       y = 'frequency')
